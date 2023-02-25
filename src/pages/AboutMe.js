88e3fd894a73fbdcbabd0fd5bd9fc534103bf8f6
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2023-02-25 15:15:10 +0700

    Update AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -7,7 +7,7 @@ export default function AboutMe(){
     return(
         <Page title="About Me">
             <Grid item xs={12} sm={6} md={6}>
-                <ComponentAbout title="Saya Annas Ismail Muhamamd saat ini saya sedang berkuliah di Universitas Bina Nusantara Jurusan Sistem Informasi. Sebelumnya terima kasih telah mengunjungi website saya semoga senang dengan karya saya. Dan bermanfaat untuk anda. Jika ada yang perlu di sampaikan bisa melalui contact dibawah ini." total="Annas Ismail Muhammad" icon='https://cdn.discordapp.com/attachments/830080342026092566/1023483858831814706/download_1.jpg' />
+                <ComponentAbout title="Assalamualikum WarohmatullahiWwabarokatu. Perkenalkan nama Annas Ismail Muhammad, saya saat ini sedang berkuliah di Universitas Bina Nusantara Jurusan Sistem Informasi. Sebelumnya terima kasih telah mengunjungi website saya, semoga senang dengan karya saya dan bermanfaat untuk anda. Jika ada yang perlu di sampaikan bisa melalui contact dibawah ini." total="Annas Ismail Muhammad" icon='https://cdn.discordapp.com/attachments/830080342026092566/1023483858831814706/download_1.jpg' />
             </Grid>
         </Page>
     );
